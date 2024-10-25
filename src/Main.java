8ab421f904e3fe4ff2865242f2c2a3a3f76b875b
anjelika A <vbfbbf@fg.com>
2024-10-25 21:07:09 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int ticket = 6000;
+        int bonus = 20;
+        int miles = ticket / bonus;
+
+        System.out.println("За покупку билета" + ticket + "руб. Ваш бонус" + miles + "бонусных миль");
+    }
+
+}
